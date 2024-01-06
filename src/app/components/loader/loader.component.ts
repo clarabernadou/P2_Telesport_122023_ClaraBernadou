77b90eb544e47fdb5c73a9e57b7707a749389cc6
Clara Bernadou <clarabernadou56690@gmail.com>
2024-01-07 00:27:51 +0100

    [add] - add a loader at page load

---
+++ src/app/components/loader/loader.component.ts
@@ -0,0 +1,20 @@
+// loader.component.ts
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../../loader.service';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent implements OnInit {
+  isLoading: boolean = false;
+
+  constructor(private loaderService: LoaderService) { }
+
+  ngOnInit(): void {
+    this.loaderService.isLoading.subscribe((loading) => {
+      this.isLoading = loading;
+    });
+  }
+}
